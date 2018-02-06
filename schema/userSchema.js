d451952af935b8a2ffd73735cd95bf3d71db1bf8
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-06 16:12:13 -0600

    adding an array of snippets

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
+var userSchema = new Schema({
   email: String,
   password: String,
   firstname: String,
   lastname: String,
   age: Number,
   city: String,
-  state: String
+  state: String,
+  snippets: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "snippet"
+    }
+  ]
 });
 
 var User = mongoose.model("User", userSchema);
