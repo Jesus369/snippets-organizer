b48b769f29616ce0c73dfb85ffe1ae9f0a8903e3
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-06 23:10:49 -0600

    CRUD user actions

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,40 @@
 const User = require("../schema/userSchema");
 
 module.exports = {
-  index: async (req, res, next) => {
-    const users = await User.find({}).then(users => {
-      res.render("homepage", { users: users });
-    });
+  newUserForm: async (req, res, next) => {
+    res.render("register");
   },
 
-  newUser: async (req, res, next) => {
+  createUser: async (req, res, next) => {
     const newUser = new User(req.body);
     const user = await newUser.save();
-    res.status(201);
+    res.redirect("/");
+  },
+
+  getUsers: async (req, res, next) => {
+    const users = await User.find({});
+    res.render("homepage", { users: users });
   },
 
   getUser: async (req, res, next) => {
-    const user = await User.findById(req.params.id).then(user => {
-      res.render("showUser", { user: user });
-    });
+    const user = await User.findById(req.params.id);
+    res.render("showUser", { user: user });
+  },
+
+  editUserPage: async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+    res.render("editUser", { user: user });
+  },
+
+  updateUser: async (req, res, next) => {
+    const { userId } = req.params;
+    const newUser = req.body;
+    const result = await User.findByIdAndUpdate(req.params.id, newUser);
+    res.redirect("/");
+  },
+
+  deleteUser: async (req, res, next) => {
+    const user = await User.findByIdAndRemove(req.params.id);
+    res.redirect("/");
   }
 };
