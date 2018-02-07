2d82a900df45c5776ca8f8853254cdfa159444ca
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-06 23:10:27 -0600

    CRUD user routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,21 @@
-const express = require("express");
 const User = require("../schema/userSchema");
 const router = require("express-promise-router")();
 
 const UsersController = require("../controllers/user");
 
-router.route("/").get(UsersController.index).post(UsersController.newUser);
-router.route("/:id").get(UsersController.getUser);
+router.route("/").get(UsersController.getUsers);
+
+router
+  .route("/register")
+  .get(UsersController.newUserForm)
+  .post(UsersController.createUser);
+
+router
+  .route("/:id")
+  .get(UsersController.getUser)
+  .put(UsersController.updateUser)
+  .delete(UsersController.deleteUser);
+
+router.route("/:id/edit").get(UsersController.editUserPage);
 
 module.exports = router;
