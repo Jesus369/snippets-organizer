2dd56a5ea9ed405ad1c64a761f50f3cc37b671a3
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-08 09:00:39 -0600

    login/delete account route

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,16 @@ const User = require("../schema/userSchema");
 const router = require("express-promise-router")();
 const UsersController = require("../controllers/user");
 
-router.route("/").get(UsersController.getUsers);
+router.route("/").get(UsersController.getAllSnippets);
 router
   .route("/register")
-  .get(UsersController.newUserForm)
-  .post(UsersController.createUser);
+  .get(UsersController.registerForm)
+  .post(UsersController.registerUser);
+
+router
+  .route("/login")
+  .get(UsersController.loginForm)
+  .post(UsersController.loginUser);
 
 router
   .route("/:id")
@@ -15,11 +20,14 @@ router
   .delete(UsersController.deleteUser);
 
 router.route("/:id/edit").get(UsersController.editUserPage);
-router.route("/:id/createSnippet").get(UsersController.createSnippetForm);
+
+router.route("/:id/cancelaccount").get(UsersController.userDelete);
 
 router
-  .route("/:id/snippets")
-  .get(UsersController.getUserSnippet)
+  .route("/:id/createSnippet")
+  .get(UsersController.createSnippetForm)
   .post(UsersController.createUserSnippet);
+
+router.route("/:id/snippets").get(UsersController.userSnippets);
 
 module.exports = router;
